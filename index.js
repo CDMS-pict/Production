c104d56f5c9007f897a9389b5db7b7611916f831
unknown <chaitanyalokhande87@gmail.com>
2022-10-29 15:34:35 +0530

    initial commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(fileUpload({
   useTempFiles: true
 }))
 // app.use(express.json());
-app.use(cors({credentials: true, origin :[ "http://localhost:3000", "http://192.168.65.34:3000"]}));
+app.use(cors({credentials: true, origin :[ "https://digital-academic-passport.herokuapp.com"]}));
 // app.use(cookieParser);
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
